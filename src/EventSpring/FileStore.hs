1a3d29b7f03ff4456b2547660968e48f648299d8
Lars Hollenbach <forestphoenix@mailbox.org>
2020-02-07 00:33:45 +0100

    Added Test for FileStore

--- src/EventSpring/FileStore.hs
+++ src/EventSpring/FileStore.hs
@@ -3,39 +3,57 @@ module EventSpring.FileStore where
 import qualified Conduit                       as C
 import           Control.Concurrent.Async
 import           Control.Concurrent.Chan
-import           Control.Monad                 (forM_)
+import           Control.Monad                 (forM_, when)
 import qualified Data.ByteString.Lazy          as BS
 import qualified Data.Conduit.Combinators      as C
 import           System.IO                     (FilePath, IOMode (..),
                                                 openBinaryFile)
+import System.Directory (doesFileExist)
 
 import           EventSpring.Common
 import           EventSpring.Internal.EventLog
 import           EventSpring.Projection
 import           EventSpring.Serialized
 import           EventSpring.Store
+import           EventSpring.TransactionT
 
-newtype FileStore = FileStore Store
+import GHC.Stack
+
+data FileStore = FileStore {
+    fsStore       :: Store,
+    fsWriteChan   :: Chan (WriteMsg AnyEvent),
+    fsWriteThread :: Async ()
+}
 
 data WriteMsg event = WriteEvents [event] | FinishWrites
 
 -- Warning: Since we are dealing with impure code here,
 -- anything more complex than trivial should be refactored.
 
-mkFileStore :: (SerializedType -> BS.ByteString -> Either String AnyEvent) -> FilePath -> Projector AnyProjId -> IO FileStore
+mkFileStore :: HasCallStack => (SerializedType -> BS.ByteString -> Either String AnyEvent) -> FilePath -> Projector AnyProjId -> IO FileStore
 mkFileStore lookupType fileName projector = do
     eventsToWrite <- newChan
 
     let eventsToChan events = writeChan eventsToWrite $ WriteEvents events
     store <- mkEmptyStore eventsToChan projector
-    C.withSourceFile fileName $ \fileConduit -> C.runConduit $
+
+    storeAlreadyPresent <- doesFileExist fileName
+    when storeAlreadyPresent $ C.withSourceFile fileName $ \fileConduit -> C.runConduit $
         fileConduit C..|
         deserializeConduit lookupType C..|
         C.awaitForever (\event -> C.liftIO $ applyEventsRaw store [event]) -- this is horribly inefficient.
 
     writeThread <- async $ writeEvents fileName eventsToWrite
 
-    pure $ FileStore store
+    pure $ FileStore store eventsToWrite writeThread
+
+closeFileStore :: FileStore -> IO ()
+closeFileStore store = do
+    writeChan (fsWriteChan store) FinishWrites
+    wait $ fsWriteThread store
+
+runFileTransaction :: FileStore -> TransactionT IO a -> IO a
+runFileTransaction fileStore = runTransaction (fsStore fileStore)
 
 writeEvents :: FilePath -> Chan (WriteMsg AnyEvent) -> IO ()
 writeEvents fileName eventsToWrite = C.runResourceT $ C.runConduit $
---
+++ test/EventSpring/FileStoreSpec.hs
@@ -0,0 +1,38 @@
+module EventSpring.FileStoreSpec where
+
+import           Control.Concurrent.Async
+import           Data.IORef
+import System.IO.Temp (withSystemTempDirectory)
+import Control.Monad (forM)
+
+import           Common
+import           EventSpring.FileStore
+import           EventSpring.Store
+import           EventSpring.TransactionT
+import           EventSpring.TypeLookup
+
+spec :: Spec
+spec = do
+    it "can read a projection after closing the file" $ property $
+      \(events :: [TestEvB]) (projIds :: [B]) -> ioProperty $ withSystemTempDirectory "canreadtest" $ \dir -> do
+        let file = (dir ++ "/store.eslog")
+
+        originalStore <- mkFileStore (lookupType deserializeInfo) file testProjector
+        originalProjections <- runFileTransaction originalStore $ do
+            record events
+            forM projIds $ \projId -> readProjection projId
+        closeFileStore originalStore
+
+        loadedStore <- mkFileStore (lookupType deserializeInfo) file testProjector
+        loadedProjections <- runFileTransaction loadedStore $ do
+            forM projIds $ \projId -> readProjection projId
+        closeFileStore loadedStore
+
+        pure $ counterexample file $ originalProjections === loadedProjections
+
+    xit "will have updated projections if the file is opened with a differen projector" $ do
+        putStrLn $ "TODO"
+
+deserializeInfo =
+    toEventLookup (mkLookup :: TypeLookup TestEvB) <>
+    toEventLookup (mkLookup :: TypeLookup TestEvA)
